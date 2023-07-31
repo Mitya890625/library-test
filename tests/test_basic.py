d369bd954803612388574f74ee5881eea29b8eac
MityaZhdan <dmitriyzhdan23@gmail.com>
2023-07-31 11:22:24 +0300

    add line to README.md-7

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -12,16 +12,22 @@ def test_read_all_empty():
 
 
 def test_create_book():
-    new_book_data = {"title": "The lord of the rings", "year": "1950", "authors": []}
+    new_book_data = {
+        "title": "The lord of the rings",
+        "year": "1950", "authors": []
+    }
     new_book_data_json = json.dumps(new_book_data)
-    resp: httpx = httpx.post(f"{BASE_URL}/books", data=new_book_data_json)
+    resp: httpx = httpx.post(f"{BASE_URL}/books", data=new_book_data_json)  # noqa 501
     assert resp.status_code == 201
 
 
 def test_create_author():
-    new_author_data = {"name": "J.R.R Tolkien", "country": "England", "books": []}
+    new_author_data = {
+        "name": "J.R.R Tolkien",
+        "country": "England", "books": []
+    }
     new_author_data_json = json.dumps(new_author_data)
-    resp: httpx = httpx.post(f"{BASE_URL}/authors", data=new_author_data_json)
+    resp: httpx = httpx.post(f"{BASE_URL}/authors", data=new_author_data_json)  # noqa 501
     assert resp.status_code == 201
 
 
@@ -34,7 +40,10 @@ def test_relation():
     resp: httpx = httpx.get(f"{BASE_URL}/books/1")
     pyth_resp = json.loads(resp.text)
     authors = pyth_resp.get("authors")
-    assert authors[0] == {"id": 1, "name": "J.R.R Tolkien", "country": "England"}
+    assert authors[0] == {"id": 1,
+                          "name": "J.R.R Tolkien",
+                          "country": "England"
+                          }
 
 
 def test_deletion():

e498b4b0da35ee2fa9a0ba8e7b3b64488af1b7b9
MityaZhdan <dmitriyzhdan23@gmail.com>
2023-07-31 13:10:03 +0300

    configure env variables

--- library_test/database.py
+++ library_test/database.py
@@ -1,11 +1,16 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Ost17@localhost:5432/books"
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       # connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
